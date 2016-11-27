9c0fa5d83d6c764a09b885f5783be1cd0fc37c18
udutta <udutta@uzzal-42-dhk.vantage.com>
2016-11-27 16:41:50 +0600

    Git test code update

---
+++ src/main/java/formreleaf/gittest/App.java
@@ -0,0 +1,13 @@
+package formreleaf.gittest;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/formreleaf/gittest/NewTest.java
@@ -0,0 +1,27 @@
+package formreleaf.gittest;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+
+public class NewTest {		
+	    private WebDriver driver;		
+		@Test (priority = 1, description = "This is check for webpage title")				
+		public void testEasy() {	
+			driver.get("http://www.guru99.com/selenium-tutorial.html");  
+			String title = driver.getTitle();				 
+			Assert.assertTrue(title.contains("Free Selenium Tutorials")); 
+			System.out.println("Passed Test");
+		}	
+		@BeforeTest
+		public void beforeTest() {	
+		    driver = new FirefoxDriver();  
+		}		
+		@AfterTest
+		public void afterTest() {
+			driver.quit();			
+		}
+}
